6e86f18a447980e004910e6e03c8dec9b4d12768
RoyalKev <34064409+RoyalKev@users.noreply.github.com>
2025-01-24 11:57:38 +0000

    Second commit apres config

--- config/staticip.js
+++ config/staticip.js
@@ -1,2 +1,2 @@
-export const StaticIP =  "http://localhost:5000/"
-//export const StaticIP =  "http://77.37.125.3:3006/"
+//export const StaticIP =  "http://localhost:5000/"
+export const StaticIP =  "http://77.37.125.3:3006/"
